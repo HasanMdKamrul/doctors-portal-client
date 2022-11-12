c513cfa9cb43187a1b94e09f4c6ed3adfa3977cd
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-12 20:41:00 +0100

    date picker added

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import { format } from "date-fns";
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
 import chair from "../../../assets/images/chair.png";
-import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
 const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
-    <div className="hero banner-bg lg:w-[1363px] lg:h-[838px]   ">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} className="rounded-lg shadow-2xl lg:w-1/2" alt="" />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <PrimaryButton>Get Started</PrimaryButton>
+    <div>
+      <div className="hero banner-bg lg:w-[1363px] lg:h-[838px]   ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className="rounded-lg shadow-2xl lg:w-1/2" alt="" />
+          <div className="bg-gray-100">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center text-secondary my-6 text-bold">
+        {selectedDate && (
+          <p>Appointment available on {format(selectedDate, "PP")} </p>
+        )}
       </div>
     </div>
   );
