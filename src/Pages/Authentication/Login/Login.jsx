6f13a41183a1292bf7917d281e11fe73a135e3da
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-13 22:37:30 +0100

    Login form design generated

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [data, setData] = useState("");
   const { register, handleSubmit } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
 
   return (
     <div>
       <div className="h-screen flex justify-center items-center">
-        <div>
-          <h1 className="text-primary text-4xl text-center font-bold">
-            Log In
-          </h1>
+        <div className="w-[385px] h-[480px] shadow-lg p-7  rounded-2xl">
+          <h1 className=" text-xl text-center font-bold">Log In</h1>
           <form
-            onSubmit={handleSubmit((data) => {
-              try {
-                setData(data);
-              } catch (error) {
-                console.log(error.message);
-              }
-            })}
+            onSubmit={handleSubmit(handleLogin)}
             className="form-control w-full "
           >
             <label className="label">
@@ -40,12 +36,27 @@ const Login = () => {
               placeholder="*******"
               className="input input-bordered w-full"
             />
+            <label className="label">
+              <span className="label-text">Forgot Password?</span>
+            </label>
             <input
               type="submit"
-              value="Submit"
-              className="btn btn-primary mt-2"
+              value="Log In"
+              className="btn btn-accent mt-2"
             />
+            <label className="label">
+              <span className="label-text">
+                New to Doctor's Portal ?{" "}
+                <Link className="text-secondary" to="/signup">
+                  Create new account
+                </Link>
+              </span>
+            </label>
           </form>
+          <div className="divider">OR</div>
+          <button className="btn btn-outline w-full">
+            CONTINUE WITH GOOGLE
+          </button>
         </div>
       </div>
     </div>
