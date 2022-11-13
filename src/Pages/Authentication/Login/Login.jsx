9805cdf346f4a7bdac5492a92e7fea6f1f0e58e3
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-13 22:07:38 +0100

    React hook form added and used

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [data, setData] = useState("");
+  const { register, handleSubmit } = useForm();
+
   return (
     <div>
-      <h1>This is login</h1>
+      <div className="h-screen flex justify-center items-center">
+        <div>
+          <h1 className="text-primary text-4xl text-center font-bold">
+            Log In
+          </h1>
+          <form
+            onSubmit={handleSubmit((data) => {
+              try {
+                setData(data);
+              } catch (error) {
+                console.log(error.message);
+              }
+            })}
+            className="form-control w-full "
+          >
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              placeholder="Email..."
+              className="input input-bordered w-full"
+            />
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="*******"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-primary mt-2"
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
